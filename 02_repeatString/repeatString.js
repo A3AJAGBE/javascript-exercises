27192ee8a4bdc6630b79c3480746f3fd61bf9f51
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2023-08-24 13:50:07 +0100

    Add two arguements to the function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(word, numOfTimes) {
+    let result = "";
+    if (numOfTimes < 0) {
+        return "ERROR";
+    } else {
+        for (let i=1; i <= numOfTimes; i++) {
+        result+=word
+        
+    }
+    return result;
+    }
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
