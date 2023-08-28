4d354e19b9390a282bb5cc3aa044af3029d987d3
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2023-08-28 10:36:31 +0100

    Add the function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a, n) {
+    let totalSum = 0;
+    if ((typeof a !== 'number') || (typeof n !== 'number') || (a < 0) || (n < 0)) {
+        return "ERROR";
+    } else {
 
+        if (a > n) {
+            for (let i = a; i >= n; i--) {
+                totalSum += i;
+            }
+        } else {
+            for (let i = a; i <= n; i++) {
+                totalSum += i;
+            }
+        }
+        return totalSum;
+    }
+    
 };
 
 // Do not edit below this line
