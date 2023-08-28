4b9f25a9a449df9daf58878b0f80d087148162af
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2023-08-28 11:04:58 +0100

    Reformat the code

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -2,11 +2,7 @@ const leapYears = function(year) {
   if (!Number.isInteger(year)) {
     return "ERROR";
   } else {
-    if ((year % 400 == 0) || ((year % 100 != 0) && (year % 4 == 0))) {
-      return true;
-    } else {
-      return false;
-    }
+    return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
   }
 };
 
